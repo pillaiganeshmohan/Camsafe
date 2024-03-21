48a4ec0e4c2e087b0c240995c0295572e1e0dc8d
Pillai Ganesh Mohan <pillaiganeshmohan@gmail.com>
2024-03-21 21:53:18 +0530

    Delete src directory

--- src/schemas/index.jsx
+++
@@ -1,8 +0,0 @@
-import * as Yup from "yup";
-
-export const signUpSchema = Yup.object({
-  name: Yup.string().min(2).max(25).required("Please enter your name"),
-  lastname: Yup.string().min(2).max(25).required("Please enter your surname"),
-  email: Yup.string().email().required("Please enter your email"),
-  message: Yup.string().min(30).max(180).required("please enter your message"),
-});

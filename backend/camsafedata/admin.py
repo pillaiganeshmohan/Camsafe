84d914b8f405b455afbe69dbd7c29ebd6ef8abfb
unknown <jannatiqbalsk@gmail.com>
2024-06-09 01:00:25 +0530

    Cleaned Database

--- backend/camsafedata/admin.py
+++ backend/camsafedata/admin.py
@@ -8,18 +8,9 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ('email', 'user_role', 'police_station_code')
     list_filter = ('is_active', 'user_role')
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'phone', 'is_active', 'created_at', 'updated_at')
-    search_fields = ('user__email', 'phone')
-    list_filter = ('is_active', 'created_at', 'updated_at')
 
-admin.site.register(Subject)
-admin.site.register(CCTVIdentityMaster)
-admin.site.register(AdminIdentity)
-admin.site.register(UserIdentity)
+
 admin.site.register(CcTVIdentityTransaction)
-admin.site.register(FeatureData)
 admin.site.register(ContactUs)
 admin.site.register(SubjectDetails)
 admin.site.register(ProImage)

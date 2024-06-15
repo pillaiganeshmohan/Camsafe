4e802b8b176ed8c275ff20dd1a536cbba5dfa75a
pillaiganeshmohan <pillaiganeshmohan@gmail.com>
2024-06-15 15:43:10 +0530

    Updated requirements.txt and README.md

--- README.md
+++ README.md
@@ -1,9 +1,51 @@
-# Version 1.0 
+# Camsafe
 
-Don't push the code directly into main branch. Create a new branch and submit your changes to that branch. Create a Compare and Pull request before pushing code into main branch.
+CAMSAFE a real-time subject face recognition is a dynamic system designed to rapidly identify and match faces against a database of known subjects. This involves curating a comprehensive subject dataset, preprocessing facial images for uniformity, and selecting an effective face recognition model. Integration with real-time face detection enables the system to operate seamlessly on pictures from live or recorded video streams. Setting appropriate recognition thresholds ensures a balance between sensitivity and specificity, and an admin interface will be implemented for authorized personnel interaction. Multiple factors like, birthmark, facial features, expressions and predicted data of different age groups for the same subject will be considered. Upon deployment, continuous monitoring and updates are crucial to maintain accuracy, and legal and ethical considerations must be addressed to comply with regulations and safeguard privacy. Robust security measures will be implemented to prevent unauthorized access, recognizing the sensitivity of the information involved. While promising for enhancing public safety, the responsible deployment of real-time subject face recognition necessitates careful ethical considerations and a commitment to privacy preservation.
 
 # Run Frontend
+
+### `cd frontend`
 
 ### `npm install`
 
 ### `npm start`
+
+## Frontend will be hosted on below link
+
+### `http://localhost:3000/`
+
+# Run Backend
+
+### `cd backend`
+
+### `virtualenv venv`
+
+### `.\venv\Scripts\activate`
+
+### `pip install -r requirements.txt`
+
+### `daphne -b 127.0.0.1 -p 8000 camsafe.asgi:application`
+
+## Backend will be hosted on below link
+
+### `http://127.0.0.1:8000/`
+
+# Run ML
+
+### `cd ml`
+
+### `virtualenv env`
+
+### `.\env\Scripts\activate`
+
+### `pip install .\dlib-19.24.99-cp312-cp312-win_amd64.whl`
+
+### `pip install -r requirements.txt`
+
+## To Register the Data
+
+### `python main.py`
+
+## To Run the Model
+
+### `python master.py`
--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -1,34 +1,51 @@
-@echo off
-
-rem This file is UTF-8 encoded, so we need to update the current code page while executing it
-for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
-    set _OLD_CODEPAGE=%%a
+@REM This file is UTF-8 encoded, so we need to update the current code page while executing it
+@for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
+    @set _OLD_CODEPAGE=%%a
 )
-if defined _OLD_CODEPAGE (
+@if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Priyanshi\OneDrive\Desktop\Internship Project\CamSafe\Camsafe\backend\venv
+@set "VIRTUAL_ENV=C:\Users\Ganesh Mohan Pillai\React Projects\Camsafe\backend\venv"
 
-if not defined PROMPT set PROMPT=$P$G
+@set "VIRTUAL_ENV_PROMPT="
+@if NOT DEFINED VIRTUAL_ENV_PROMPT (
+    @for %%d in ("%VIRTUAL_ENV%") do @set "VIRTUAL_ENV_PROMPT=%%~nxd"
+)
 
-if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
-if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
+@if defined _OLD_VIRTUAL_PROMPT (
+    @set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    @if not defined PROMPT (
+        @set "PROMPT=$P$G"
+    )
+    @if not defined VIRTUAL_ENV_DISABLE_PROMPT (
+        @set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+    )
+)
+@if not defined VIRTUAL_ENV_DISABLE_PROMPT (
+    @set "PROMPT=(%VIRTUAL_ENV_PROMPT%) %PROMPT%"
+)
 
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(venv) %PROMPT%
+@REM Don't use () to avoid problems with them in %PATH%
+@if defined _OLD_VIRTUAL_PYTHONHOME @goto ENDIFVHOME
+    @set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+:ENDIFVHOME
 
-if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
-set PYTHONHOME=
+@set PYTHONHOME=
 
-if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
-if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
+@REM if defined _OLD_VIRTUAL_PATH (
+@if not defined _OLD_VIRTUAL_PATH @goto ENDIFVPATH1
+    @set "PATH=%_OLD_VIRTUAL_PATH%"
+:ENDIFVPATH1
+@REM ) else (
+@if defined _OLD_VIRTUAL_PATH @goto ENDIFVPATH2
+    @set "_OLD_VIRTUAL_PATH=%PATH%"
+:ENDIFVPATH2
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(venv) 
+@set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
 
-:END
-if defined _OLD_CODEPAGE (
+@if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
-    set _OLD_CODEPAGE=
+    @set _OLD_CODEPAGE=
 )
